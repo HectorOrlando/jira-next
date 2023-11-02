9cae3aeffc4dc026f47a5566016d02dc528310d1
HectorOrlando <14orgui@gmail.com>
2023-11-02 15:43:35 +0100

    añadir Material UI y configurar thema

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
+const serverRuntimeConfig = {  port: 3007};
+
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig
 }
 
 module.exports = nextConfig
